29a0e29cf4d40819dfa3d2f8579eaec16ebaf0c7
Michael Coxe <mcoxe@me.com>
2025-09-28 01:56:05 -0400

    Updating the logic and functions of the menus

--- Scripts/game_menu.gd
+++ Scripts/game_menu.gd
@@ -4,6 +4,7 @@ const RULES_SCENE = preload("res://Scene/Rules.tscn")
 const SCORING_SCENE = preload("res://Scene/Scoring.tscn")
 const STATS_SCENE = preload("res://Scene/Scoring.tscn")
 @onready var start_over_dialog: MarginContainer = $ColorRect/StartOverDialog
+@onready var margin_container: MarginContainer = $ColorRect/MarginContainer
 
 func _on_close_button_pressed() -> void:
 	queue_free()
@@ -22,10 +23,12 @@ func _on_rules_button_pressed() -> void:
 	add_child(scene)
 
 func _on_start_over_button_pressed() -> void:
+	margin_container.hide()
 	start_over_dialog.visible = true
 
 func _on_start_over_dialog_confirmed() -> void:
 	get_tree().change_scene_to_file("res://Scene/Start.tscn")
 
 func _on_start_over_dialog_canceled() -> void:
+	margin_container.visible = true
 	start_over_dialog.hide()
